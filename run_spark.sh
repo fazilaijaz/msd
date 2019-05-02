57dd88959bf7b5c4d36c007cb9c6e9ed0e761e06
fazilaijaz <50165884+fazilaijaz@users.noreply.github.com>
2019-05-02 14:24:29 +0800

    Update run_spark.sh
    spark2-submit to spark-submit

--- run_spark.sh
+++ run_spark.sh
@@ -4,7 +4,7 @@ timestamp=$(date +%Y-%m-%d' '%H:%M:%S)
 echo " $timestamp : run_spark script started."
 
 #run spark app to calculate the average
-spark2-submit --conf spark.yarn.executor.memoryOverhead=4096 --executor-memory 4G --driver-memory 4G --executor-cores 4 --driver-cores 4 --conf spark.default.parallelism=12 spark_app.py
+spark-submit --conf spark.yarn.executor.memoryOverhead=4096 --executor-memory 4G --driver-memory 4G --executor-cores 4 --driver-cores 4 --conf spark.default.parallelism=12 spark_app.py
 
 #extract data to local for visualization
 hive -e 'set hive.cli.print.header=true; select * from msd.nutrition' | sed 's/[\t]/|/g' > visualization/nutrition.csv
